1e6979ab34baf9c7f65a163b4f8507df36448221
Ahsan Joyo <75047284+ahsanejoyo@users.noreply.github.com>
2026-09-10 16:41:57 -0500

    9/10 Update ssd1306_min.py
    Many variables have been changed to constants and certain parts have been refactored to improve memory footprint

--- code/ssd1306_min.py
+++ code/ssd1306_min.py
@@ -3,55 +3,66 @@
 
 import framebuf
 
+# config commands
+DISP_OFF = const(0xAE)         # Turn display off
+HORIZ_MEM_ADD = const(0x20)   # Memory addressing mode set to horizontal
+CTRL_BYTE = const(0x00)   # 0x00 signifies control byte to diffrentiate from visual/pixel data
+START_LINE = const(0x40)       # Starting line
+SEG_REMAP = const(0xA1)        # Segment re-map (Column 127 mapped to SEG0)
+MLTPLX_RTO = const(0xA8)       # Set multiplex ratio
+COM_OUT_SCAN = const(0xC8)     # COM output scan direction
+DISP_OFFSET1 = const(0xD3)
+DISP_OFFSET2 = const(0x00)     # Display offset
+COM_PIN1 = const(0xDA)
+COM_PIN2 = const(0x02)
+COM_PIN3 = const(0x12)         # COM pins configuration
+DISP_CLK_DR1 = const(0xD5)
+DISP_CLK_DR2 = const(0x80)     # Display clock divide ratio / oscillator frequency
+PRE_CHARGE1 = const(0xD9)
+PRE_CHARGE2 = const(0xF1)      # Pre-charge Period
+VCOMH_DESEL_LVL1 = const(0xDB)
+VCOMH_DESEL_LVL2 = const(0x30) # VCOMH deselect level
+CONTRAST_CTRL1 = const(0x81) 
+CONTRAST_CTRL2 = const(0xFF)   # Contrast control (Set to max brightness)
+FULL_DISP_ON = const(0xA4)     # ENTIRE display ON (Resume from RAM)
+NORM_DISP = const(0xA6)        # Normal display (non-inverted) 
+CHRG_PUMP_CTRL1 = const(0x8D)
+CHRG_PUMP_CTRL2 = const(0x14)  # Charge pump control (Enables charge pump)
+DISP_ON = const(0xAF)          # Display ON
+
+
 # BUILT-IN MINIMAL SSD1306 DRIVER (I2C)
 class SSD1306(framebuf.FrameBuffer):
     def __init__(self, width, height):
 
-        # Variables
         self.width, self.height = width, height
         self.pages = self.height // 8 # Sections of screen are divided into 8 pixel tall "Pages", varies based on screen height
         self.buffer = bytearray(self.pages * self.width) # Buffer of size: pages*screen width (1 byte = 8 pixels)
 
-        # Tuple w/ defined hex commands (change to const for further memory optimization?)
-        config_cmds = (
-            0xAE,        # Turn display off
-            0x20, 0x00,  # Memory addressing mode set to horizontal
-            0x40,        # Starting line
-            0xA1,        # Segment re-map (Column 127 mapped to SEG0)
-            0xA8, self.height - 1, # Set multiplex ratio
-            0xC8,        # COM output scan direction
-            0xD3, 0x00,  # Display offset
-            0xDA, 0x02 if self.width > 2 * self.height else 0x12, # COM pins configuration
-            0xD5, 0x80,  # Display clock divide ratio / oscillator frequency
-            0xD9, 0xF1,  # Pre-charge Pperiod
-            0xDB, 0x30,  # VCOMH deselect level
-            0x81, 0xFF,  # Contrast control (Set to max brightness)
-            0xA4,        # ENTIRE display ON (Resume from RAM)
-            0xA6,        # Normal display (non-inverted) 
-            0x8D, 0x14,  # Charge pump control (Enables charge pump)
-            0xAF         # Display ON
-        )
 
         # MP's framebuffer is initialized, framebuffer is set to Monochrome & Vertical Least Significant Bit
         super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB) 
 
-        # For loop cycles iterates through tuple containing various hex & standard commands to configure OLED controller hardware
-        for cmd in (config_cmds):
+        # Loop iterates through various hex commands to configure OLED controller hardware
+        for cmd in (DISP_OFF, HORIZ_MEM_ADD, CTRL_BYTE, START_LINE, SEG_REMAP, MLTPLX_RTO, self.height - 1, COM_OUT_SCAN, 
+                    DISP_OFFSET1, DISP_OFFSET2, COM_PIN1, COM_PIN2 if self.width > 2 * self.height else COM_PIN3, DISP_CLK_DR1,  
+                    DISP_CLK_DR2, PRE_CHARGE1, PRE_CHARGE2, VCOMH_DESEL_LVL1, VCOMH_DESEL_LVL2, CONTRAST_CTRL1, CONTRAST_CTRL2,
+                    FULL_DISP_ON, NORM_DISP, CHRG_PUMP_CTRL1, CHRG_PUMP_CTRL2, DISP_ON):
             self.write_cmd(cmd) # Current command is written directly to screen/display chip
 
     def show(self): # Function to take drawn pixels sitting in RAM and display them on screen
         self.write_cmd(0x21); self.write_cmd(0); self.write_cmd(self.width - 1)
         self.write_cmd(0x22); self.write_cmd(0); self.write_cmd(self.pages - 1)
-        self.write_framebuf() # Sends byte array from RAM to display chip
+        self.write_framebuf() # sends byte array from RAM to display chip
 
-class SSD1306_I2C(SSD1306): # Graphic capabilities from previous function are transferred here and specified to work w/ I2C
+class SSD1306_I2C(SSD1306): # SSD1306 class called here and specified to work w/ I2C
 
     def __init__(self, width, height, i2c, addr=0x3C):
         self.i2c, self.addr = i2c, addr
         super().__init__(width, height)
 
     def write_cmd(self, cmd): # Function to set configuration info to display chip
-        self.i2c.writeto(self.addr, bytearray([0x00, cmd])) # 0x00 signifies control byte to diffrentiate from visual/pixel data
+        self.i2c.writeto(self.addr, bytearray([CTRL_BYTE, cmd])) 
 
     def write_framebuf(self): # Bit block transfer of pixel data to screen 
         self.i2c.writeto(self.addr, b'\x40' + self.buffer) # 0x40 signifies following info is visual/pixel data
